92d11ca5d095adc0575fcb9790d3b302843a6c6a
Paddymck2320 <56718903+Paddymck2320@users.noreply.github.com>
2021-10-06 10:53:55 +0100

    v2

---
+++ Flaskproject/Flaskproject/app.py
@@ -0,0 +1,113 @@
+from flask import Flask, render_template, request
+
+import tweepy as tp
+import pandas as pd
+import json
+from twitter_auth import *
+
+auth = tp.OAuthHandler(API_KEY, API_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+api = tp.API(auth)
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def homepage():
+    return render_template('index.html')
+
+
+@app.route('/about', methods=['GET'])
+def aboutpage():
+    return render_template('about.html')
+
+@app.route('/chart')
+def chart():
+    df = pd.read_csv('static/CSV/Book1.csv', header=None)
+    series = {}
+    drilldown = {}
+
+    series['name'] = 'Tweets'
+    series['colorByPoint'] = 'true'
+
+    drilldown['series'] = []
+    current_value = None
+    data = []
+    series_data = []
+    value = 0
+
+    for row in df.iterrows():
+
+        ent = row[1]
+
+        id = ent[0]
+        cat = ent[1]
+        val = ent[2]
+
+        if current_value == None:
+            current_value = id
+
+        if id != current_value:
+            list_item = {}
+            series_list_item = {}
+
+            list_item['name'] = current_value
+            list_item['id'] = current_value
+            list_item['data'] = data
+
+            series_list_item['name'] = current_value
+            series_list_item['y'] = value
+            series_list_item['drilldown'] = current_value
+
+            series_data.append(series_list_item)
+
+            data = []
+            value = 0
+            drilldown['series'].append(list_item)
+            current_value = id
+
+        data.append([cat, val])
+        value += val
+
+    list_item = {}
+    series_list_item = {}
+
+    list_item['name'] = current_value
+    list_item['id'] = current_value
+    list_item['data'] = data
+
+    series_list_item["name"] = current_value
+    series_list_item["y"] = value
+    series_list_item["drilldown"] = current_value
+
+    series_data.append(series_list_item)
+
+    data = []
+    value = 0
+    drilldown['series'].append(list_item)
+
+    series['data'] = series_data
+
+    series = json.dumps(series)
+    drilldown = json.dumps(drilldown)
+
+    print(drilldown)
+    print(series)
+    return render_template('chart.html', series=series, drilldown = drilldown)
+
+
+@app.route('/tweets', methods=['POST'])
+def tweetspage():  # put application's code here
+
+    if request.method == 'POST':
+        query = request.form['query']
+        tweets = api.search(query)
+
+        return render_template('results.html', query=tweets)
+
+    return render_template('404.html')
+
+
+if __name__ == '__main__':
+    app.run()
